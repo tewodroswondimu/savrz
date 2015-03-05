0a8aac7328fe33de222f4aadfb32f7613d5cb9ea
Tewodros Wondimu <tewodros.wr@gmail.com>
2015-03-05 09:06:35 -0800

    Initial Commit

---
+++ SAVRZ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SAVRZ
+//
+//  Created by Tewodros Wondimu on 3/5/15.
+//  Copyright (c) 2015 Tewodros Wondimu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SAVRZ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SAVRZ
+//
+//  Created by Tewodros Wondimu on 3/5/15.
+//  Copyright (c) 2015 Tewodros Wondimu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
